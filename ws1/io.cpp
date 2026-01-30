c804a8b0219cb142bb58032cdbdc2b1abf69c576
peronamyqueen <isaabishev2006@gmail.com>
2026-01-30 08:10:50 -0500

    Added citation to ws1

--- ws1/io.cpp
+++ ws1/io.cpp
@@ -1,91 +1,93 @@
-#include <iostream>
+/*
+Workshop 1
+Filename: io.cpp
+-----------------------------------------------------------
+Author: Issa Abishev
+Student number: 175980234
+Email: iabishev@myseneca.ca
+Subject: OOP244NAA
+-----------------------------------------------------------
+I have done all the coding by myself
+-----------------------------------------------------------
+*/
+
 #include "io.h"
 #include "cstr.h"
+#include <iostream>
 
 using namespace std;
 namespace seneca {
 
-   void read(char* name) {
-      cout << "name>\n ";
-      cin >> name;
-   }
+void read(char *name) {
+    cout << "name>\n ";
+    cin >> name;
+}
 
-   void print(long long phone) {
-      long long area = phone / 10000000;
-      long long mid  = (phone / 10000) % 1000;
-      long long last = phone % 10000;
+void print(long long phone) {
+    long long area = phone / 10000000;
+    long long mid = (phone / 10000) % 1000;
+    long long last = phone % 10000;
 
-      cout << "("
-           << area << ") ";
+    cout << "(" << area << ") ";
 
-      cout.fill('0');
-      cout.width(3);
-      cout << mid << "-";
+    cout.fill('0');
+    cout.width(3);
+    cout << mid << "-";
 
-      cout.width(4);
-      cout << last;
+    cout.width(4);
+    cout << last;
 
-      cout.fill(' ');
-   }
+    cout.fill(' ');
+}
 
-   void print(const PhoneRec& rec, size_t& row, const char* filter) {
-      bool match = true;
+void print(const PhoneRec &rec, size_t &row, const char *filter) {
+    bool match = true;
 
-      if (filter) {
-         match =
-            strstr(rec.name, filter) ||
-            strstr(rec.lastname, filter);
-      }
+    if (filter) {
+        match = strstr(rec.name, filter) || strstr(rec.lastname, filter);
+    }
 
-      if (match) {
-         cout << row << ": "
-              << rec.name << " "
-              << rec.lastname << " ";
-         print(rec.phone);
-         cout << endl;
-         row++;
-      }
-   }
+    if (match) {
+        cout << row << ": " << rec.name << " " << rec.lastname << " ";
+        print(rec.phone);
+        cout << endl;
+        row++;
+    }
+}
 
-   bool read(PhoneRec& rec, FILE* fptr) {
-      return fscanf(
-         fptr,
-         "%s %s %lld",
-         rec.name,
-         rec.lastname,
-         &rec.phone
-      ) == 3;
-   }
+bool read(PhoneRec &rec, FILE *fptr) {
+    return fscanf(fptr, "%s %s %lld", rec.name, rec.lastname, &rec.phone) == 3;
+}
 
-   void print(PhoneRec* recs[], size_t size, const char* filter) {
-      size_t row = 1;
-      for (size_t i = 0; i < size; i++) {
-         print(*recs[i], row, filter);
-      }
-   }
+void print(PhoneRec *recs[], size_t size, const char *filter) {
+    size_t row = 1;
+    for (size_t i = 0; i < size; i++) {
+        print(*recs[i], row, filter);
+    }
+}
 
-   void setPointers(PhoneRec* ptrs[], PhoneRec recs[], size_t size) {
-      for (size_t i = 0; i < size; i++) {
-         ptrs[i] = &recs[i];
-      }
-   }
+void setPointers(PhoneRec *ptrs[], PhoneRec recs[], size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        ptrs[i] = &recs[i];
+    }
+}
 
-   void sort(PhoneRec* ptrs[], size_t size, bool byLastName) {
-      for (size_t i = 0; i < size - 1; i++) {
-         for (size_t j = i + 1; j < size; j++) {
+void sort(PhoneRec *ptrs[], size_t size, bool byLastName) {
+    for (size_t i = 0; i < size - 1; i++) {
+        for (size_t j = i + 1; j < size; j++) {
             int cmp;
             if (byLastName) {
-               cmp = strcmp(ptrs[i]->lastname, ptrs[j]->lastname);
+                cmp = strcmp(ptrs[i]->lastname, ptrs[j]->lastname);
             } else {
-               cmp = strcmp(ptrs[i]->name, ptrs[j]->name);
+                cmp = strcmp(ptrs[i]->name, ptrs[j]->name);
             }
 
             if (cmp > 0) {
-               PhoneRec* temp = ptrs[i];
-               ptrs[i] = ptrs[j];
-               ptrs[j] = temp;
+                PhoneRec *temp = ptrs[i];
+                ptrs[i] = ptrs[j];
+                ptrs[j] = temp;
             }
-         }
-      }
-   }
+        }
+    }
 }
+} // namespace seneca
--- ws1/io.h
+++ ws1/io.h
@@ -1,32 +1,44 @@
+/*
+Workshop 1
+Filename: io.h
+-----------------------------------------------------------
+Author: Issa Abishev
+Student number: 175980234
+Email: iabishev@myseneca.ca
+Subject: OOP244NAA
+-----------------------------------------------------------
+I have done all the coding by myself
+-----------------------------------------------------------
+*/
+
 #ifndef SENECA_IO_H
 #define SENECA_IO_H
 
 #include <cstddef>
-#include <cstdio> 
+#include <cstdio>
 
 namespace seneca {
 
-   struct PhoneRec {
-      char name[16];
-      char lastname[26];
-      long long phone;
-   };
+struct PhoneRec {
+        char name[16];
+        char lastname[26];
+        long long phone;
+};
 
-   void read(char* name);
+void read(char *name);
 
-   void print(long long phone);
+void print(long long phone);
 
-   void print(const PhoneRec& rec, size_t& row, const char* filter = nullptr);
+void print(const PhoneRec &rec, size_t &row, const char *filter = nullptr);
 
-   bool read(PhoneRec& rec, FILE* fptr);
+bool read(PhoneRec &rec, FILE *fptr);
 
-   void print(PhoneRec* recs[], size_t size, const char* filter = nullptr);
+void print(PhoneRec *recs[], size_t size, const char *filter = nullptr);
 
-   void setPointers(PhoneRec* ptrs[], PhoneRec recs[], size_t size);
+void setPointers(PhoneRec *ptrs[], PhoneRec recs[], size_t size);
 
-   void sort(PhoneRec* ptrs[], size_t size, bool byLastName);
+void sort(PhoneRec *ptrs[], size_t size, bool byLastName);
 
-}
+} // namespace seneca
 
-#endif 
-
+#endif
